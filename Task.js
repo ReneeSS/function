9aa5001f8ddb8c172d45ad0fa22b790703d2bdad
ReneeSS <reneeffs@gmail.com>
2024-04-06 21:13:55 +0300

    Task 02

---
+++ Task.js
@@ -0,0 +1,24 @@
+function sayHello(name) {
+  console.log(`Привет, ${name}`);
+}
+
+sayHello("Антон");
+
+
+
+function returnNeighboringNumbers(number) {
+  const arr = [];
+
+  arr.push(number - 1);
+  arr.push(number);
+  arr.push(number + 1);
+  console.log(arr);
+}
+
+returnNeighboringNumbers(0);
+
+// Task 03
+
+function getMathResult() {
+  
+}
